b5d169f857265f69070fc026e14c3e154ae4390e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-12-21 04:53:25 +0100

    add sync event and old option

--- test.js
+++ test.js
@@ -55,3 +55,48 @@ test('no opts', t => {
   })
   db.put('foo', 'bar')
 })
+
+test('ready event', t => {
+  t.plan(5)
+  const db = level('ready')
+  db.put('a', 'a')
+  db.put('b', 'b')
+  setTimeout(() => {
+    let i = 0
+    const stream = new Live(db)
+    stream.on('data', op => {
+      i++
+      if (i === 1) t.equal(op.key, 'a')
+      if (i === 2) t.equal(op.key, 'b')
+      if (i === 3) t.equal(op.key, 'c')
+      if (i === 4) t.equal(op.key, 'd')
+    })
+    stream.on('sync', () => {
+      t.equal(i, 2)
+    })
+    setTimeout(() => {
+      db.put('c', 'c')
+      db.put('d', 'd')
+    }, 100)
+  }, 10)
+})
+
+test('skip old', t => {
+  t.plan(2)
+  const db = level('skip old')
+  db.put('a', 'a')
+  db.put('b', 'b')
+  let i = 0
+  setTimeout(() => {
+    const stream = new Live(db, { old: false })
+    stream.on('data', op => {
+      i++
+      if (i === 1) t.equal(op.key, 'c')
+      if (i === 2) t.equal(op.key, 'd')
+    })
+  }, 50)
+  setTimeout(() => {
+    db.put('c', 'c')
+    db.put('d', 'd')
+  }, 100)
+})
